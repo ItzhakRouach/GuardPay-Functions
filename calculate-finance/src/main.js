1d5b7302b0014d1ad5eee4c7938044cd559810cb
Itzhak Rouach <tzachiruash12345@gmail.com>
2026-01-19 05:50:26 +0200

    merge two calculate functions to one folder

--- calculate-finance/src/main.js
+++ calculate-finance/src/main.js
@@ -5,6 +5,7 @@ export default async ({ req, res, log, error }) => {
     return res.json({ error: "No Data Provided" }, 400);
   }
 
+  //check for how data arive and destruct like should
   let body;
   try {
     body = typeof req.body === "string" ? JSON.parse(req.body) : req.body;
@@ -12,46 +13,70 @@ export default async ({ req, res, log, error }) => {
     error("JSON Parse Error: " + e.message);
     return res.json({ error: "Invalid JSON format" }, 400);
   }
+
+  const { action, payload } = body;
   try {
-    //extract data
-    const { startTime, endTime, baseRate, travelRate, type, user_id } = body;
-    log(`Calculating shift for user: ${user_id || "annonymous"}`);
+    if (action === "CALCULATE_SALARY") {
+      const {
+        regularPay,
+        extraPay,
+        travelPay,
+        training_pay = 0,
+        vacation_pay = 0,
+      } = payload;
 
-    // initilize result var to hold the calculation of the shift
-    let result;
-
-    if (type === "training" || type === "vacation") {
-      result = {
-        total_amount: Number(baseRate * 8),
-        reg_hours: 0,
-        extra_hours: 0,
-        reg_pay_amount: 0,
-        extra_pay_amount: 0,
-        travel_pay_amount: type === "training" ? Number(travelRate) : 0,
-        h100_hours: 0,
-        h125_extra_hours: 0,
-        h150_extra_hours: 0,
-        h175_extra_hours: 0,
-        h200_extra_hours: 0,
-        h150_shabat: 0,
-        base_rate: baseRate,
-        is_training: type === "training",
-        is_vacation: type === "vacation",
-        start_time: startTime,
-        end_time: endTime,
-      };
-    } else {
-      result = calculateShiftPay(startTime, endTime, baseRate, travelRate);
-      result.is_training = false;
-      result.is_vacation = false;
-      result.start_time = startTime;
-      result.end_time = endTime;
-      result.base_rate = Number(baseRate);
+      const result = calculateSalary(
+        regularPay,
+        extraPay,
+        travelPay,
+        training_pay,
+        vacation_pay,
+      );
+      log("Salary calculate successfully");
+      return res.json(result);
     }
-    log("Shift calculate successfully");
-    // return the result to the user
-    return res.json(result);
-  } catch (err) {
+
+    if (action === "CALCULATE_SHIFT") {
+      const { startTime, endTime, baseRate, travelRate, type, user_id } =
+        payload;
+      log(`Calculating shift for user: ${user_id || "annonymous"}`);
+
+      // initilize result var to hold the calculation of the shift
+      let result;
+
+      if (type === "training" || type === "vacation") {
+        result = {
+          total_amount: Number(baseRate * 8),
+          reg_hours: 0,
+          extra_hours: 0,
+          reg_pay_amount: 0,
+          extra_pay_amount: 0,
+          travel_pay_amount: type === "training" ? Number(travelRate) : 0,
+          h100_hours: 0,
+          h125_extra_hours: 0,
+          h150_extra_hours: 0,
+          h175_extra_hours: 0,
+          h200_extra_hours: 0,
+          h150_shabat: 0,
+          base_rate: baseRate,
+          is_training: type === "training",
+          is_vacation: type === "vacation",
+          start_time: startTime,
+          end_time: endTime,
+        };
+      } else {
+        result = calculateShiftPay(startTime, endTime, baseRate, travelRate);
+        result.is_training = false;
+        result.is_vacation = false;
+        result.start_time = startTime;
+        result.end_time = endTime;
+        result.base_rate = Number(baseRate);
+      }
+      log("Shift calculate successfully");
+      // return the result to the user
+      return res.json(result);
+    }
+  } catch (e) {
     error("Calculation error: " + err.message);
     return res.json(
       { error: "Internal Server Error", details: err.message },
@@ -60,6 +85,61 @@ export default async ({ req, res, log, error }) => {
   }
 };
 
+// Calculate Monthly Salary of User
+const calculateSalary = (
+  regularPay,
+  extraPay,
+  travelPay,
+  training_pay = 0,
+  vacation_pay = 0,
+) => {
+  const bruto = regularPay + extraPay + travelPay + training_pay + vacation_pay;
+
+  // --- 1. Pensia calculation 7% regulat hours , 7% extra hours 5% travel  ---
+  const pensia = regularPay * 0.07 + extraPay * 0.07 + travelPay * 0.05;
+
+  // --- 2. Bituah Leumi & Health (Split Rate) ---
+  const thresholdBL = 7522;
+  let bituahLeumiAndHealth = 0;
+
+  if (bruto <= thresholdBL) {
+    bituahLeumiAndHealth = bruto * 0.035;
+  } else {
+    bituahLeumiAndHealth = thresholdBL * 0.035 + (bruto - thresholdBL) * 0.12;
+  }
+
+  // --- 3. Income Tax ---
+  let grossTax = 0;
+  if (bruto <= 7010) {
+    grossTax = bruto * 0.1;
+  } else if (bruto <= 10060) {
+    grossTax = 710 + (bruto - 7010) * 0.14;
+  } else if (bruto <= 16150) {
+    grossTax = 710 + 427 + (bruto - 10060) * 0.2;
+  } else {
+    grossTax = 710 + 427 + 1218 + (bruto - 16150) * 0.31;
+  }
+
+  // --- 4. Tax Credit Points (Nekudot Zichuy) ---
+  const points = 2.25;
+  const creditValue = points * 242;
+  const finalIncomeTax = Math.max(0, grossTax - creditValue);
+
+  // --- 5. Final Neto ---
+  const totalDeductions = pensia + bituahLeumiAndHealth + finalIncomeTax;
+  const neto = bruto - totalDeductions;
+
+  return {
+    bruto,
+    pensia,
+    bituahLeumiAndHealth,
+    incomeTax: finalIncomeTax,
+    neto,
+    totalDeductions,
+  };
+};
+
+// Calculate User shift
 const calculateShiftPay = (startTime, endTime, baseRate, travelRate) => {
   const start = new Date(startTime);
   let end = new Date(endTime);
