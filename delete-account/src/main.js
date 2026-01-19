1319ad9bf9a9d49fec87aba9755eb91d15e3a104
Itzhak Rouach <tzachiruash12345@gmail.com>
2026-01-19 06:01:01 +0200

    update the fuction

--- delete-account/src/main.js
+++ delete-account/src/main.js
@@ -1,16 +1,16 @@
 // initilize appwrite sdk
 import { Client, Databases, Query, Users } from "node-appwrite";
 
-const client = new Client();
-client
-  .setEndpoint(process.env.APPWRITE_FUNCTION_ENDPOINT)
-  .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-  .setKey(process.env.APPWRITE_API_KEY);
-
-const databases = new Databases(client);
-const users = new Users(client);
-
 export default async ({ req, res, log, error }) => {
+  const client = new Client();
+  client
+    .setEndpoint(process.env.APPWRITE_FUNCTION_ENDPOINT)
+    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+    .setKey(process.env.APPWRITE_API_KEY);
+
+  const databases = new Databases(client);
+  const users = new Users(client);
+
   //check for data arrive
   if (!req.body) {
     error("Request body is missing!");
@@ -27,51 +27,52 @@ export default async ({ req, res, log, error }) => {
   }
   try {
     const { userId } = body;
-    const userPref = await databases.listDocuments(
-      process.env.DATABASE_ID,
-      process.env.USERS_PREFS_ID,
-      [Query.equal("user_id", userId)],
-    );
-
-    const userShifts = await databases.listDocuments(
-      process.env.DATABASE_ID,
-      process.env.SHIFTS_HISTORY_ID,
-      [Query.equal("user_id", userId)],
-    );
+    const [userPref, userShifts] = await Promise.all([
+      databases.listDocuments(
+        process.env.DATABASE_ID,
+        process.env.USERS_PREFS_ID,
+        [Query.equal("user_id", userId)],
+      ),
+      databases.listDocuments(
+        process.env.DATABASE_ID,
+        process.env.SHIFTS_HISTORY_ID,
+        [Query.equal("user_id", userId)],
+      ),
+    ]);
 
     // now after fetching we delete all the records if existed
 
     // Delete user Pref records
-    if (userPref.documents.length !== 0) {
-      await Promise.all(
-        userPref.documents.map((doc) =>
-          databases.deleteDocument(
-            process.env.DATABASE_ID,
-            process.env.USERS_PREFS_ID,
-            doc.$id,
-          ),
+    const deleteTasks = [
+      ...userPref.documents.map((doc) =>
+        databases.deleteDocument(
+          process.env.DATABASE_ID,
+          process.env.USERS_PREFS_ID,
+          doc.$id,
         ),
-      );
-    }
+      ),
+      ...userShifts.documents.map((doc) =>
+        databases.deleteDocument(
+          process.env.DATABASE_ID,
+          process.env.SHIFTS_HISTORY_ID,
+          doc.$id,
+        ),
+      ),
+    ];
 
-    //Delete all user shifts record
-    if (userShifts.documents.length !== 0) {
-      await Promise.all(
-        userShifts.documents.map((doc) =>
-          databases.deleteDocument(
-            process.env.DATABASE_ID,
-            process.env.SHIFTS_HISTORY_ID,
-            doc.$id,
-          ),
-        ),
-      );
-    }
+    await Promise.all(deleteTasks);
+    log("Documents deleted successfully.");
+
     // after deleted all records we can move on to delete the user
     await users.delete({ userId: `${userId}` });
-    return context.res.json({ message: "All data deleted successfully" });
+    log("User account deleted successfully.");
+    return res.json({ message: "All data deleted successfully" });
   } catch (err) {
     log(err);
-    error("Error happend: " + e.message);
-    return res.json({ error: "unable to delete the account" }, 400);
+    error("Error happened: " + err.message);
+    return res.json(
+      { error: "Unable to delete the account", details: err.message },
+      500,
+    );
   }
 };
